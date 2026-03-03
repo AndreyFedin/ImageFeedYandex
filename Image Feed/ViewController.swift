5b4e56206f4b48fb5421478c6ac529870e8f1841
Andrey Fedin <powermig29@gmail.com>
2026-03-03 13:37:57 +0300

    Initial Commit

---
+++ Image Feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Feed
+//
+//  Created by PowerMiG29 on 03.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
