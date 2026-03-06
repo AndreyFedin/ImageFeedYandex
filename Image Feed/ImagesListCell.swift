d3ab2050d088516d535d5ccb3d4d3191f4350d0a
Andrey Fedin <powermig29@gmail.com>
2026-03-06 15:28:42 +0300

    Финал

--- Image Feed/ImagesListCell.swift
+++ Image Feed/ImagesListCell.swift
@@ -9,4 +9,15 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    private let contentInsets = UIEdgeInsets(top: 4, left: 0, bottom: 4, right: 0)
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = bounds.inset(by: contentInsets)
+    }
 }
