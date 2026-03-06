baf294bf74f869387ed0af34acb5acb8d46be902
Andrey Fedin <powermig29@gmail.com>
2026-03-06 13:17:22 +0300

    add table and cell

---
+++ Image Feed/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by PowerMiG29 on 04.03.2026.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
